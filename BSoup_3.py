1b60b2de310b1491bece0bf0786aacf901c71aa0
Erfanx <irfansyed479@gmail.com>
2020-09-24 02:40:30 +0600

    Scrap infromation from the verge

--- BSoup_3.py
+++ BSoup_3.py
@@ -1,10 +1,16 @@
 import bs4
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('https://www.theverge.com/science').text
 
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('BSoup_3.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Headline', 'Writer', 'Month', 'Date'])
 
 for para in soup.find_all('div', class_='c-entry-box--compact__body'):
     try:
@@ -15,16 +21,15 @@ for para in soup.find_all('div', class_='c-entry-box--compact__body'):
         print(subtitle)
 
         time = para.div.span.time.text
-        month = time.split(" ")[10]
-        print(month)
+        mon = time.split(' ')[10]
+        print(mon)
 
-        date = time.split(" ")[11]
-        date = date.split("/")[0]
-
-        print(date)
-
-        print()
+        date = time.split(' ')[11]
+        date = date.split('/')[0]
     except Exception as e:
-        date = time.split(" ")[12]
-        date = date.split("/")[0]
-        print(date)
+        date = time.split(' ')[12]
+        date = date.split('/')[0]
+
+    print(date)
+    print()
+    csv_writer.writerow([headline, subtitle, mon, date])
